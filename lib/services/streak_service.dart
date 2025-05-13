c8316aaf6ffe73541b5a6862de9b21f1ff563a75
itshardlight <unishmhrjan@gmail.com>
2025-05-13 22:16:04 +0545

    logic improvemenet

--- lib/services/streak_service.dart
+++ lib/services/streak_service.dart
@@ -194,4 +194,25 @@ class StreakService {
     };
   }
 
- 
+  Future<void> resetStreak() async {
+    try {
+      if (isUserLoggedIn) {
+        await _streakRef!.set({
+          'current_streak': 0,
+          'daily_streak': 0,
+          'last_workout_date': null,
+          'reached_milestone': false,
+          'updated_at': FieldValue.serverTimestamp(),
+        }, SetOptions(merge: true));
+      }
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setInt('workout_streak', 0);
+      await prefs.setInt('daily_streak', 0);
+      await prefs.remove('last_workout_date');
+      await prefs.setBool('reached_milestone', false);
+      await prefs.setInt('daily_workouts', 0);
+    } catch (e) {
+      debugPrint('Error resetting streak: $e');
+    }
+  }
+}
